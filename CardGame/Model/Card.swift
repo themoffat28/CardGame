df2d5318029e1db7006df8765824470b586dff78
themoffat28 <bthemoffat@gmail.com>
2016-10-25 09:27:31 -0600

    Created project and ignore files.

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Moffat, Braeden on 10/25/16.
+//  Copyright © 2016 Moffat, Braeden. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Moffat, Braeden on 10/25/16.
+//  Copyright © 2016 Moffat, Braeden. All rights reserved.
+//
+
+import Foundation
