0a3fc4cb1334f3f22c6c78dccd06de567f865d6a
themoffat28 <bthemoffat@gmail.com>
2016-10-27 09:24:14 -0600

    Updated and replaced storyboard controller.

---
+++ CardGame/Controller/CardGameController.swift
@@ -0,0 +1,14 @@
+//
+//  CardGameController.swift
+//  CardGame
+//
+//  Created by Moffat, Braeden on 10/27/16.
+//  Copyright © 2016 Moffat, Braeden. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    
+}
