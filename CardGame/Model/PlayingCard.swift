a186a4e51cdd24c7aee3329fa61945f5a6898329
themoffat28 <bthemoffat@gmail.com>
2016-10-27 09:05:46 -0600

    Calling parent-class methods in sub-class.

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -26,13 +26,64 @@ class PlayingCard : Card
     {
         get
         {
-                return self.suit
+            return self.suit
         }
         set(suit)
         {
             self.suit = suit
         }
     }
+    
+    private var color : UIColor
+    {
+        get
+        {
+            return self.color
+        }
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    
+    private var frontImage : UIImage
+    {
+        get
+        {
+            return self.frontImage
+        }
+        set(frontImage)
+        {
+            self.frontImage = frontImage
+        }
+    }
+    
+    override init()
+    {
+        super.init()
+        frontImage = UIImage()
+        color = UIColor()
+        rank = 0
+        suit = String()
+    }
+        
+    override func toString() -> String
+    {
+        let facing : String
+        if self.isUp()
+        {
+            facing = " facing up."
+        }
+        else
+        {
+            facing = " facing down."
+        }
+        
+        let description = "This Playing Card with a back image of \(self.getBackImage()) has a face value of \(rank), the color \(color), and is the suit \(suit) \(facing)."
+     
+        return description
+    }
+    
     func getrank() -> Int
     {
         return self.rank
