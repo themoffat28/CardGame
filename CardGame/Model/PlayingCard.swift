4672f777feb10bd9a8b64bb3a6c012f75dd8a2bf
themoffat28 <bthemoffat@gmail.com>
2016-10-31 09:08:52 -0600

    Assigned a blank white front image of the cards and began process of defining rank.

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -61,10 +61,20 @@ class PlayingCard : Card
     override init()
     {
         super.init()
-        frontImage = UIImage()
-        color = UIColor()
+        //frontImage = UIImage()
+        color = UIColor.whiteColor()
         rank = 0
         suit = String()
+    }
+    
+    init(withRank: Int, ofSuit: String)
+    {
+        super.init()
+        //frontImage = UIImage()
+        color = UIColor.whiteColor()
+        
+        rank = withRank
+        suit = ofSuit
     }
         
     override func toString() -> String
