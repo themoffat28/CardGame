1217b3e6a6dd5d870ff2bcac6e6410c28f6c62a6
themoffat28 <bthemoffat@gmail.com>
2016-11-02 09:25:50 -0600

    Updated UmLet diagram.

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -11,23 +11,16 @@ import UIKit
 class PlayingCard : Card
 {
     internal var rank : Int
-  
-    
     internal var suit : String
-   
-    
     internal var color : UIColor
-   
-    
     internal var frontImage : UIImage
  
-    
     override init()
     {
-        frontImage = UIImage()
-        color = UIColor.whiteColor()
-        rank = 0
-        suit = String()
+        self.frontImage = UIImage()
+        self.color = UIColor.whiteColor()
+        self.rank = 0
+        self.suit = String()
         super.init()
     }
     
